fe71de36c09973602aa65106c420d832b4b1559f
cgardn <christopher.gardn@gmail.com>
2020-02-04 16:57:37 -0500

    add booking passenger information with nested forms, add bookings show page, add bookings index page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,24 +2,45 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
-    @booking.flight = Flight.find(bookingparams[:flightid])
+    @booking.flight = Flight.find(booking_form_params[:flightid])
     
-    bookingparams[:passengercount].to_i.times do
+    booking_form_params[:passengercount].to_i.times do
       @booking.passengers.build
     end
   end
 
   def create
     @booking = Booking.new
-    @booking.flight = Flight.find(bookingparams[:flightid])
+    @booking.flight = Flight.find(booking_params[:flightid])
+    
+    # feels hacky, having to index the attribute hash, but whatevs?
+    booking_params[:passengers_attributes].each do |p|
+      @booking.passengers.build(p[1])
+    end
+
+    if @booking.save
+      redirect_to @booking
+    else
+      puts "**--!!--** Didn't save!! **--!!--**"
+      render :new
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def index
+    @bookings = Booking.all
   end
 
   private
-    def bookingparams
-      params.require(:booking).permit(:flightid, :passengercount)
+    def booking_form_params
+      params.require(:booking_form).permit(:flightid, :passengercount)
+    end
+
+    def booking_params
+      params.require(:booking).permit(:flightid, passengers_attributes: [:id, :name, :email])
     end
 
 end
